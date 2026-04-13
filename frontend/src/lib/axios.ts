6310238c6fdda6f04b3f723502e1b12de4c3a67b
omar2550 <omr222000@gmail.com>
2026-04-13 18:56:18 +0200

    add env for frontend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "production" ? "" : "http://localhost:5000/api";
+  import.meta.env.MODE === "production" ? "" : import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
